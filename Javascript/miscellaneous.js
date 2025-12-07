dcb248c5ef72cba987a34e7cedf4ebb56c61ac14
Yogesh <yogeshyoki03@gmail.com>
2025-12-07 12:58:33 +0530

    Add files via upload
    Javascript files

---
+++ Javascript/miscellaneous.js
@@ -0,0 +1,22 @@
+// Boolean data type
+
+let isMale = true;
+
+console.log(typeof isMale);
+
+let condition = 5<3;
+
+console.log(condition);
+
+// null value
+
+let age = null;
+console.log(age);
+
+// undefined value
+
+let DOB
+
+console.log(DOB);
+
+
---
+++ Javascript/string.js
@@ -0,0 +1,21 @@
+//             STRING DATA TYPE
+
+let intro = 'Hi This is Yogesh';
+let channel = "This is yogesh's channel";
+
+let quote = 'He said "I am Bat Man"'; 
+
+let paragraph = `This is Bat Man's home. Bruce Wayne said "I'm Bat Man"`;
+
+// embedding in string
+
+let embed = `1+2 = ${intro}`;
+
+console.log(embed);
+
+let name = "yogesh";
+
+let embedding = `My name is: ${name}`;
+
+console.log(embedding);
+
