69b43e7015e88ae1897cdbeebc82eecd41887aa6
Yogesh2526-Space <yogeshyoki03@gmail.com>
2025-12-08 13:50:29 +0530

    js

---
+++ Javascript/operations.js
@@ -0,0 +1,36 @@
+// 7 + 11 = 18
+// + is an operator
+//7, 11 are operands
+
+//unary
+
+let x = 1;
+x = -x;
+console.log(x);
+
+//binary +,-,/,*,%,**
+
+let y=12, z=6;
+
+sum = a+b;
+console.log(sum); 
+
+// string concatenation
+ let aa=12, bb=6;
+let sum =+aa + +bb;
+console.log(sum);
+
+// Modify in place
+// eg: a=a+5; instead we use a+=5;
+let a=5;
+
+a += 5;
+console.log(a); 
+
+// increment and decrement
+// we can use the operator before or after the variable
+let w=24;
+
+w--;
+
+console.log(x); 
