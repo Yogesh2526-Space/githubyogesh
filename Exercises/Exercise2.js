19f8ee95acb5a16c3c51a0d5ae9623dd70398ad9
Yogesh <yogeshyoki03@gmail.com>
2026-02-08 15:37:58 +0530

    nodejs

--- Exercises/Exercise2.js
+++ Exercises/Exercise2.js
@@ -1,45 +1,41 @@
-// OR and AND
+const fs = require("fs");
 
-let age1 = 22;
-let Gender = 'F';
-
-if(age >=21 || (age>=18 && Gender == 'F'))
-{
-    console.log("Candidate is Eligible for Voting");
-}
-else{
-
-console.log("Candidate is Not Eligible for Voting");
-} 
-
-// Null coleasing
-
-let age = null;
-let current_age = age ?? 22;
-
-console.log(current_age);
-
-
-// factorial
-
-let n1 = 5;
-
-let ans1 = 1;
-
-for(i=1; i<=n; i++)
-{
-    ans1 = ans1 * i;
-    console.log(ans1);
-} 
-
-    // using while loop
-
-    let n = 5;
-    let i=1;
-    let ans= 1;
-
-    while(i<=n){
-        ans = ans * i;
-        i++;
+fs.mkdir("./docs1", (err) =>{
+    if(err){
+        console.log(err.message);
     }
-    console.log(ans);
+    else{
+        console.log("Directory Created");
+    }
+})
+
+fs.writeFile("./docs1/astronomy.txt","It's a mind-blown study",(err)=>{
+    if(err){
+        console.log(err.message);
+    }
+    else{
+        console.log("File Created");
+    }
+})
+
+fs.readFile("./docs1/astronomy.txt", (err,data)=>{
+    if(err){
+        console.log(err.message);
+    }
+    else{
+        console.log(data.toString());
+
+    }
+})
+
+
+if(fs.existsSync("./docs")){
+fs.unlink("./docs1/astronomy.txt",(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Successfully unliked");
+    }
+})
+}
